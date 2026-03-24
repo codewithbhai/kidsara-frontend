08f4dccbec4e270b87a12864056223e4d7349d00
codewithbhai <mzabzulfikaransari643@gmail.com>
2026-03-24 18:54:22 +0530

    code pushed and released build on 24 march

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AddItem from "./components/AddItem";
 import Dashboard from "./components/Dashboard";
